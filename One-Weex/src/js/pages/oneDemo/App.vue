b2d786d28930342c59df73e478849a1c0193fb15
zhengxiaobo <zhengxiaobo@jianbing.com>
2018-02-27 17:31:31 +0800

    first commit

---
+++ One-Weex/src/js/pages/oneDemo/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <tab-bar :tabItems="items" @tabBarOnClick="tabbarOnClick"></tab-bar>
+</template>
+
+<script>
+    import tabBar from './common/bar'
+    import  { tabCofig } from "./config";
+
+    export default {
+        data(){
+            return {
+                items: tabCofig
+            }
+        },
+        components: {
+            'tab-bar' : tabBar,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ One-Weex/src/js/pages/oneDemo/config.js
@@ -0,0 +1,29 @@
+export const tabCofig = [{
+    index: 0,
+    title: 'ONE',
+    titleColor: '#000000',
+    icon: '',
+    image: 'http://gtms03.alicdn.com/tps/i3/TB1LEn9MpXXXXaUXpXX9t7RGVXX-46-46.png',
+    selectedImage: 'http://gtms02.alicdn.com/tps/i2/TB1qysbMpXXXXcnXXXX9t7RGVXX-46-46.png',
+    src: ``,
+    visibility: 'hidden'
+},{
+    index: 1,
+    title: '阅读',
+    titleColor: '#000000',
+    icon: '',
+    image: 'http://gtms03.alicdn.com/tps/i3/TB1LEn9MpXXXXaUXpXX9t7RGVXX-46-46.png',
+    selectedImage: 'http://gtms02.alicdn.com/tps/i2/TB1qysbMpXXXXcnXXXX9t7RGVXX-46-46.png',
+    src: ``,
+    visibility: 'hidden'
+},{
+    index: 2,
+    title: '音乐',
+    titleColor: '#000000',
+    icon: '',
+    image: 'http://gtms03.alicdn.com/tps/i3/TB1LEn9MpXXXXaUXpXX9t7RGVXX-46-46.png',
+    selectedImage: 'http://gtms02.alicdn.com/tps/i2/TB1qysbMpXXXXcnXXXX9t7RGVXX-46-46.png',
+    src: ``,
+    visibility: 'hidden'
+}
+]
